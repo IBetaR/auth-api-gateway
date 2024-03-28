55fc6d345f1583a284b10b3cdee1a6fc0adea3f2
Ilich Betancourt Rangel <ilichbr@gmail.com>
2024-03-28 00:21:45 -0300

    Still Define open endpoints and endpoints that require rate limiting

--- gateway-service/src/main/java/com/ibetar/config/RouteValidator.java
+++ gateway-service/src/main/java/com/ibetar/config/RouteValidator.java
@@ -11,7 +11,19 @@ public class RouteValidator {
     public static final List<String> openEndpoints = List.of(
             "/auth/register"
     );
+
     public Predicate<ServerHttpRequest> isSecured =
             serverHttpRequest -> openEndpoints.stream()
                     .noneMatch(uri -> serverHttpRequest.getURI().getPath().contains(uri));
+
+    // Define open endpoints and endpoints that require rate limiting
+    public static final List<String> rateLimitedEndpoints = List.of(
+            "/users/"
+    );
+
+    // Predicate to check if the request should be rate-limited
+    public Predicate<ServerHttpRequest> shouldRateLimit =
+            serverHttpRequest -> rateLimitedEndpoints.stream()
+                    .anyMatch(uri -> serverHttpRequest.getURI().getPath().contains(uri));
+
 }
