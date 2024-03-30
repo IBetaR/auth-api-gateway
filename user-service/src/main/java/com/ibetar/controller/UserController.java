29dd229bc899a09a6f36dd8f280a231b50b865a7
Ilich Betancourt Rangel <ilichbr@gmail.com>
2024-03-29 23:47:17 -0300

    Adding new inventory-service

--- user-service/src/main/java/com/ibetar/controller/UserController.java
+++ user-service/src/main/java/com/ibetar/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ibetar.controller;
 
 import com.ibetar.entity.UserVO;
 import com.ibetar.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +16,14 @@ public class UserController {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
     private final UserService userService;
     @PostMapping
+    @Operation(summary = "The user can register")
     public ResponseEntity<UserVO> register(@RequestBody UserVO user) {
         LOGGER.info("Registering user with email: " + user.getEmail());
         return ResponseEntity.ok(userService.register(user));
     }
 
     @GetMapping("secured")
+    @Operation(summary = "The user retrieve data securely")
     public ResponseEntity<String> getSecuredData() {
         return ResponseEntity.ok("This is a secured and authenticated response");
     }
