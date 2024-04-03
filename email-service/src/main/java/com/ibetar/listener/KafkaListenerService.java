57312d436a38e94ac3ddd5c7b2a742ac3cce2f9e
Ilich Betancourt Rangel <ilichbr@gmail.com>
2024-04-03 20:50:07 -0300

    Add Kafka Streams binding

--- email-service/src/main/java/com/ibetar/listener/KafkaListenerService.java
+++ email-service/src/main/java/com/ibetar/listener/KafkaListenerService.java
@@ -1,19 +1,35 @@
 package com.ibetar.listener;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
 
 @Service
 @Slf4j
 public class KafkaListenerService {
-    @KafkaListener(topics = "mails")
-    public void listenMailMessages(String message) {
-        var msgParts = message.split(":");
-        log.info("Kafka listener service is performing message {} with content: {}.",
-                msgParts[0],
-                msgParts[1]
-        );
+
+    @Bean
+    public Consumer<KStream<Object, String>> listenMessages() {
+        return input -> {
+            input.foreach((k, v) -> {
+                var msgParts = v.split(":");
+                log.info("Kafka listener service is performing message {} with content: {}.",
+                        msgParts[0],
+                        msgParts[1]
+                );
+            });
+        };
     }
+//    @KafkaListener(topics = "mails")
+//    public void listenMailMessages(String message) {
+//        var msgParts = message.split(":");
+//        log.info("Kafka listener service is performing message {} with content: {}.",
+//                msgParts[0],
+//                msgParts[1]
+//        );
+//    }
 
 }
