86446d8d978df7c9b9bd88a40920dfd355714222
Ilich Betancourt Rangel <ilichbr@gmail.com>
2024-04-05 21:44:14 -0300

    Cleaning unused code

--- gateway-service/src/main/java/com/ibetar/config/GatewayConfig.java
+++ gateway-service/src/main/java/com/ibetar/config/GatewayConfig.java
@@ -3,12 +3,10 @@ package com.ibetar.config;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableHystrix
 public class GatewayConfig {
     private final AuthenticationFilter filter;
     public GatewayConfig(AuthenticationFilter filter) { this.filter = filter; }
@@ -40,38 +38,5 @@ public class GatewayConfig {
     public RedisRateLimiter redisRateLimiter() {
         return new RedisRateLimiter(1, 1, 1);
     }
-
-
-//    @Bean
-//    public RouteLocator routes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("user-service", r -> r.path("/users/**")
-//                                .filters(f -> f.filter(filter))
-//                                .uri("lb://user-service"))
-//                .route("auth-service", r -> r.path("/auth/**")
-//                        .filters(f -> f.filter(filter))
-//                        .uri("lb://auth-service"))
-//                .build();
-//    }
-
-
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route(
-//                        "r1", r -> r.host("**.ibetarsync.com")
-//                        .and()
-//                        .path("/baeldung")
-//                        .uri("http://ibetarsync.com")
-//                )
-//                .route(
-//                        "myOtherID",r -> r.host("**.ibetarsync.com")
-//                        .and()
-//                        .path("/myOtherRouting")
-//                        .filters(f -> f.prefixPath("/myPrefix"))
-//                        .uri("http://othersite.com")
-//                        )
-//                .build();
-//    }
 
 }
